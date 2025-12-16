6f06835c1d05dc8ce401a3199f6f4962beb48810
Josh Addington <josh.addington@method.com>
2025-12-16 13:03:34 -0500

    Add login

--- lib/share_web/router.ex
+++ lib/share_web/router.ex
@@ -18,6 +18,7 @@ defmodule ShareWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/login", AuthController, :login
   end
 
   # Other scopes may use custom stacks.
