0fcc58c18c372694f26ecb5bcfa36621786ce3e4
Josh Addington <josh.addington@method.com>
2026-01-14 10:45:37 -0500

    Add seed command

--- lib/share/release.ex
+++ lib/share/release.ex
@@ -13,6 +13,24 @@ defmodule Share.Release do
     end
   end
 
+  def seed do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(repo, fn repo ->
+          seed_script = priv_path(repo, "seeds.exs")
+
+          if File.exists?(seed_script) do
+            IO.puts("Running seed script..")
+            Code.eval_file(seed_script)
+          else
+            IO.puts("No seed script found at #{seed_script}")
+          end
+        end)
+    end
+  end
+
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
@@ -26,5 +44,9 @@ defmodule Share.Release do
     # Many platforms require SSL when connecting to the database
     Application.ensure_all_started(:ssl)
     Application.ensure_loaded(@app)
+  end
+
+  defp priv_path(_repo, filename) do
+    Path.join([:code.priv_dir(@app), "repo", filename])
   end
 end
